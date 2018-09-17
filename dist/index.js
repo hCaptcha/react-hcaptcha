314f8849cba694b2e35e2af7e87c0c169b6015c5
ali neer <ali@alineer.com>
2018-09-17 19:06:31 -0400

    ready for testing and making a real shippable demo

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _HCaptcha = require('./component/HCaptcha');
+
+var _HCaptcha2 = _interopRequireDefault(_HCaptcha);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_reactDom2.default.render(_react2.default.createElement(
+  'div',
+  null,
+  _react2.default.createElement(
+    'h1',
+    null,
+    'HCaptcha React Demo'
+  ),
+  _react2.default.createElement(
+    'div',
+    null,
+    _react2.default.createElement(_HCaptcha2.default, null)
+  )
+), document.getElementById('app'));
+
+module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: path.join(__dirname, "./src/index.html"),
+    filename: "./index.html"
+}); 
 
 module.exports = {
   entry: './src/index.js',
@@ -20,7 +26,8 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    htmlWebpackPlugin
   ],
   devServer: {
     contentBase: './dist',
