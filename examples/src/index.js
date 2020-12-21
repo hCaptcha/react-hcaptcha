3f93f1c59fff577b015f9627977a65804aa0e014
Sergiu Danalachi <danalachi.sergiu@gmail.com>
2020-12-21 11:08:51 +0200

    Add github actions CI, upgrade to babel 7 and init tests
    Remove deprecated stage-2 as of babel 7

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const {render} = require('react-dom');
-const HCaptcha = require('../../src/');
+const HCaptcha = require('../../dist/');
 
 class ReactDemo extends React.Component {
 
