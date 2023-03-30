22d7fdc4e33b3ec900b1c1b36f8101eba08f784b
zoryana94 <zoryanavakh@gmail.com>
2023-03-30 09:52:26 +0300

    chores: Fix the tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
+"use strict";
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-export default {
+module.exports = {
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
 
