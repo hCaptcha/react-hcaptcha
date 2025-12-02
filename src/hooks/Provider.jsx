ed8c18ea5135aaa64b41ceb01b7c0b89c3548415
joao-imi <johnnymastering@gmail.com>
2025-12-02 12:49:10 -0300

    feat: hooks

---
+++ src/hooks/Provider.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useRef, useState } from "react";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
+import { HCaptchaContext } from "./Context";
+
+export const HCaptchaProvider = ({
+  sitekey = null,
+  invisible = false,
+  theme = "light",
+  rqdata = null,
+  languageOverride = null,
+  onVerify,
+  onError,
+  children,
+}) => {
+  const hcaptchaRef = useRef(null);
+
+  const [ready, setReady] = useState(false);
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleReady = () => {
+    console.log("hCaptcha loaded and ready");
+    setReady(true);
+  };
+
+  const handleError = (error) => {
+    console.error("hCaptcha error: ", error);
+  };
+
+  const handleExpire = () => {
+    console.warn("hCaptcha token expired");
+    setToken(null);
+  };
+
+  const handleVerify = (token) => {
+    setToken(token);
+    onVerify && onVerify(token);
+  };
+
+  const executeInstance = async (config = {}) => {
+    try {
+      if (!ready) {
+        throw new Error("hCaptcha not ready");
+      }
+
+      if (token) {
+        resetInstance();
+      }
+
+      const { response } = await hcaptchaRef.current.execute({
+        async: true,
+        ...(config.rqdata ? { rqdata: config.rqdata } : {}),
+      });
+
+      setToken(response);
+      
+      return response;
+    } catch (error) {
+      setError(error);
+      onError && onError(error);
+    }
+  };
+
+  const resetInstance = () => {
+    hcaptchaRef?.current?.resetCaptcha();
+  };
+
+  useEffect(() => {
+    if (rqdata) {
+      /* Note: rqdata must be unique per session */
+      hcaptchaRef?.current?.setData(rqdata);
+    }
+  }, [rqdata]);
+
+  return (
+    <HCaptchaContext.Provider
+      value={{
+        sitekey,
+        error,
+        token,
+        ready,
+        executeInstance,
+        resetInstance,
+      }}
+    >
+      {children}
+      <HCaptcha
+        sitekey={sitekey}
+        size={invisible ? "invisible" : "normal"}
+        theme={theme}
+        languageOverride={languageOverride}
+        onReady={handleReady}
+        onVerify={handleVerify}
+        onExpire={handleExpire}
+        onError={handleError}
+        ref={hcaptchaRef}
+      ></HCaptcha>
+    </HCaptchaContext.Provider>
+  );
+};
